ddb0cb751bc73ec34456ff3f55a0858a4941bb6c
Torbjörn Lönnemark <tobbez@ryara.net>
2022-04-14 01:24:01 +0200

    Update metrics table generator
    Make sure all possible state labels for per-torrent metrics are
    included.

--- gen_metrics_table.py
+++ gen_metrics_table.py
@@ -38,6 +38,8 @@ def get_labels(text):
         if key.startswith("deluge_torrent_"):
             res[key]["name"] = set()
             res[key]["hash"] = set()
+            # 'state' label for per-torrent metrics
+            res[key]["state"] = { "downloading", "seeding", "paused", "checking", "queued", "error", "active", "moving", "allocating" }
     return res
 
 
